dbf98cb9dbd63dd1416635acc2c81bcc40fc239a
jpires-n <luckyjoey13@gmail.com>
2025-01-05 14:50:28 -0300

    primeiros arquivos da minitalk

---
+++ minitalk.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpires-n <jpires-n@student.42.fr>          #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025-01-05 17:47:26 by jpires-n          #+#    #+#             */
+/*   Updated: 2025-01-05 17:47:26 by jpires-n         ###   ########.rio      */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <stdarg.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <signal.h>
+
+# ifndef SLEEP_TIME
+#  define SLEEP_TIME 100
+# endif
+
+
+
+#endif
